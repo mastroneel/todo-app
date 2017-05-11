695da8915f48199777ddc4797234444d0becd0bd
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-11 13:13:39 +0800

    Add completed and created timestamps

---
+++ playground/moment-examples.js
@@ -0,0 +1,12 @@
+var moment = require('moment');
+
+console.log(moment().format());
+
+var now = moment();
+
+console.log('Current timestamp', now.unix());
+
+
+var timestamp = 1494465663;
+var currentMoment = moment.unix(timestamp);
+console.log('current moment', currentMoment.format('MMMM Do, YYYY h:mm A'));
