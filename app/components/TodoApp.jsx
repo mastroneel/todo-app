092ad356848c697934e845e11b1add93ecd943d0
Lauren Mastroni <laurenmastroni@gmail.com>
2017-05-13 15:56:36 +0800

    Refactor

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -31,16 +31,6 @@ var TodoApp = React.createClass({
       ]
     });
   },
-  handleToggle: function (id) {
-    var updatedTodos = this.state.todos.map((todo) => {
-      if (todo.id === id) {
-        todo.completed = !todo.completed;
-        todo.completedAt = todo.completed ? moment().unix() : undefined;
-      }
-        return todo;
-    });
-    this.setState({todos: updatedTodos});
-  },
   handleSearch: function (showCompleted, searchText) {
     this.setState({
       showCompleted: showCompleted,
@@ -58,7 +48,7 @@ var TodoApp = React.createClass({
           <div className="small-11 medium-6 large-5 small-centered columns">
             <div className="container">
               <TodoSearch onSearch={this.handleSearch}/>
-              <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+              <TodoList/>
               <AddTodo onAddTodo={this.handleAddTodo}/>
             </div>
           </div>
